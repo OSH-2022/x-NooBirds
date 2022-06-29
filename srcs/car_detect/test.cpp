1651abb62a900f95116ef025e6505df7da0f19b7
npz7yyk <1127168778@qq.com>
2022-06-29 10:57:24 +0800

    demo for group menbers

---
+++ srcs/car_detect/test.cpp
@@ -0,0 +1,25 @@
+#include "includes.h"
+#include "detect_const.h"
+#include "timer.h"
+#include "coordinate.h"
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, const char** argv) {
+	Coordinate space(0);
+	printTime();
+	while (true) {
+		space.run();
+		auto data = space.getData();
+		printf(
+			"%d %d, %d %d, %d %d, %d %d, %d\n",
+			data.time.sec, data.time.msec,
+			data.cars[0].x, data.cars[0].y,
+			data.cars[1].x, data.cars[1].y,
+			data.cars[2].x, data.cars[2].y,
+			data.dangerObj.size()
+		);
+	}
+	return 0;
+}
