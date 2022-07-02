91b8545fe2fa13b8594f4374962501ee89de0b0b
Vincentove <linhuahuang@mail.ustc.edu.cn>
2022-07-02 10:56:46 +0800

    add a wrapper for heuristic scheduler

--- srcs/main/sched_thread.cpp
+++ srcs/main/sched_thread.cpp
@@ -55,3 +55,207 @@ const PackageWithVel &RVOScheduler::getNewData() {
 RVOScheduler::~RVOScheduler() {
     ;
 }
+
+HeuristicScheduler::HeuristicScheduler() {
+    ;
+}
+
+HeuristicScheduler::~RVOScheduler() {
+    ;
+}
+
+mutex &HeuristicScheduler::getNewDataMutex() {
+    return this->newDataMutex;
+}
+
+void HeuristicScheduler::setData(const PackageWithVel &d) {
+    data = d;
+}
+
+const PackageWithVel &HeuristicScheduler::getNewData() {
+    lock_guard<mutex> guard(newDataMutex);
+    return this->newData;
+}
+
+void HeuristicScheduler::schedule() {
+    long double x_tmp[OBJ_NUM] = {0.0};
+    long double y_tmp[OBJ_NUM] = {0.0};
+    long double vx_tmp[OBJ_NUM] = {0.0};
+    long double vy_tmp[OBJ_NUM] = {0.0};
+    long double adj_coe = 1.0 - adj_step_size;
+    int adjusted_car = 0;
+    int updated_car = 0;
+    int is_safe = 1;
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    int l = 0;
+    int need_adjust = 0;
+    int need_update = 0;
+    int ret = 0;
+
+    for (i = 0; i < OBJ_NUM; i++) {
+        x_tmp[i] = this->data.cars[i].x();
+        y_tmp[i] = this->data.cars[i].y();
+        vx_tmp[i] = this->data.vels[i].x();
+        vy_tmp[i] = this->data.vels[i].y();
+    }
+
+    do {
+        need_adjust = 0;
+
+        ret = 0;
+        // initial safety detection
+        for (j = 0; j < OBJ_NUM - 1; j++) {
+            for (k = j + 1; k < OBJ_NUM; k++) {
+                long double coe = safety_coe;
+                if (corner_case[j][k] == 1) {
+                    ret = unsafe_state_detector(j, k, x_tmp[j], y_tmp[j], vx_tmp[j], vy_tmp[j], x_tmp[k], y_tmp[k], vx_tmp[k], vy_tmp[k], safety_coe);
+                    if (ret == 0) {
+                        corner_case[j][k] = 0;
+                    }
+                    else {
+                        coe = 1.0;
+                    }
+                }
+                ret = unsafe_state_detector(j, k, x_tmp[j], y_tmp[j], vx_tmp[j], vy_tmp[j], x_tmp[k], y_tmp[k], vx_tmp[k], vy_tmp[k], coe);
+                if (ret == 1) {
+                    need_adjust = 1;
+                    break;
+                }
+            }
+            if (ret == 1) {
+                break;
+            }
+        }
+
+        if (ret == 1) {
+            int success = 0;
+            adj_coe = 1.0 - adj_step_size;
+            for (i = 0; i < adj_times; i++) {
+                ret = unsafe_state_detector(j, k, x_tmp[j], y_tmp[j], adj_coe * vx_tmp[j], adj_coe * vy_tmp[j], x_tmp[k], y_tmp[k], vx_tmp[k], vy_tmp[k], safety_coe);
+                if (ret == 0) {
+                    success = 1;
+                    adjusted_car = j;
+                    is_adjust[j] = 1;
+                    break;
+                }
+                else {
+                    adj_coe -= adj_step_size;
+                }
+            }
+
+            if (success == 0) {
+                adj_coe = 1.0 - adj_step_size;
+                for (i = 0; i < adj_times; i++) {
+                    ret = unsafe_state_detector(j, k, x_tmp[j], y_tmp[j], vx_tmp[j], vy_tmp[j], x_tmp[k], y_tmp[k], adj_coe * vx_tmp[k], adj_coe * vy_tmp[k], safety_coe);
+                    if (ret == 0) {
+                        success = 1;
+                        adjusted_car = k;
+                        is_adjust[k] = 1;
+                        break;
+                    }
+                    else {
+                        adj_coe -= adj_step_size;
+                    }
+                }
+            }
+
+            if (success == 0) {
+                adj_coe = 0.001;
+                long double velocity_j = pow(vx_tmp[j], 2) + pow(vy_tmp[j], 2);
+                long double velocity_k = pow(vx_tmp[k], 2) + pow(vy_tmp[k], 2);
+                corner_case[j][k] = 1;
+                long double coe;
+                coe = 1.0;
+                ret = unsafe_state_detector(j, k, x_tmp[j], y_tmp[j], vx_tmp[j], vy_tmp[j], x_tmp[k], y_tmp[k], adj_coe * vx_tmp[k], adj_coe * vy_tmp[k], coe);
+                if (ret == 0) {
+                    adjusted_car = k;
+                    is_adjust[k] = 1;
+                    slow_car[j][k] = j;
+                }
+                else {
+                    ret = unsafe_state_detector(j, k, x_tmp[j], y_tmp[j], adj_coe * vx_tmp[j], adj_coe * vy_tmp[j], x_tmp[k], y_tmp[k], vx_tmp[k], vy_tmp[k], coe);
+                    if (ret == 0) {
+                        adjusted_car = j;
+                        is_adjust[j] = 1;
+                        slow_car[j][k] = k;
+                    }
+                    else { // TO BE DONE
+
+                    }
+                }
+            }
+        }
+
+        if (need_adjust == 1) {
+            for (i = 0; i < OBJ_NUM; i++) {
+                if (i == adjusted_car) {
+                    vx_tmp[i] = adj_coe * vx_tmp[i];
+                    vy_tmp[i] = adj_coe * vy_tmp[i];
+                }
+            }
+        }
+    }
+    while (need_adjust == 1);
+
+    long double vx_save;
+    long double vy_save;
+    do
+    {
+        need_update = 0;
+        for (l = 0; l < OBJ_NUM; l++) {
+            if (is_adjust[l] == 0) {
+                continue;
+            }
+            else {
+                for (i = 0; i < OBJ_NUM; i++) {
+                    if (i == l) {
+                        long double vel_angle;
+                        vel_angle = this->vel_angle[i];
+                        vx_save = vx_tmp[i];
+                        vy_save = vy_tmp[i];
+                        vx_tmp[i] = initial_vel[i] * cos(vel_angle);
+                        vy_tmp[i] = initial_vel[i] * sin(vel_angle);
+                    }
+                }
+
+                ret = 0;
+                for (j = 0; j < OBJ_NUM - 1; j++) {
+                    for (k = j + 1; k < OBJ_NUM; k++) {
+                        long double coe;
+                        if (corner_case[j][k] == 1 && ((j != l && k != l) || (l == slow_car[j][k]))) {
+                            coe = 1.0;
+                        }
+                        else {
+                            coe = safety_coe * 2.0;
+                        }
+                        ret = unsafe_state_detector(j, k, x_tmp[j], y_tmp[j], vx_tmp[j], vy_tmp[j], x_tmp[k], y_tmp[k], vx_tmp[k], vy_tmp[k], coe);
+                        if (ret == 1) {
+                            break;
+                        }
+                    }
+                    if (ret == 1) {
+                        break;
+                    }
+                }
+
+                if (ret == 0) {
+                    need_update = 1;
+                    is_adjust[l] = 0;
+                    break;
+                }
+                else {
+                    vx_tmp[l] = vx_save;
+                    vy_tmp[l] = vy_save;
+                }
+            }
+        }
+    }
+    while (need_update == 1);
+
+    for (i = 0; i < OBJ_NUM; i++) {
+        this->newData.vels[i].x_ = vx_tmp[i];
+        this->newData.vels[i].y_ = vy_tmp[i];
+    }
+}
--- srcs/main/sched_thread.h
+++ srcs/main/sched_thread.h
@@ -4,6 +4,17 @@
 #include "../car_detect/coordinate.h"
 #include "../scheduler/schedulerRVO2/src/RVO.h"
 
+const long double pi = 3.14159265358979323846;
+const long double adj_step_size = 0.097;
+const int adj_times = 10;
+
+#define OBJ_NUM       3
+#define PRINT_INTERVAL      2000
+#define TIME_BET_SCHE       30
+#define SCHE_INTERVAL       1400
+#define CLOCKWISE           0
+#define COUNTER_CLOCKWISE   1
+
 enum sched_method {RVO2, vel_factor};
 
 typedef struct DataPackageWithVel {
@@ -31,3 +42,53 @@ private:
     RVO::RVOSimulator *sim;
     std::vector<RVO::Vector2> goals;
 };
+
+class HeuristicScheduler {
+public:
+    HeuristicScheduler();
+    ~HeuristicScheduler();
+
+    mutex& getNewDataMutex();
+    void setData(const PackageWithVel &);
+
+    const PackageWithVel& getNewData();
+    void schedule();
+
+private:
+    std::mutex newDataMutex;
+    PackageWithVel data, newData;
+
+    int is_adjust[OBJ_NUM] = {0};
+    long double initial_vel[OBJ_NUM] = {0};
+    long double obj_radius[OBJ_NUM] = {200.0, 200.0, 200.0};
+    long double safety_coe = 1.5;
+
+    long double corner_radius = 200.0;
+
+    long double rect_center[2] = {0.0, 0.0};
+    long double rect_len = 1000.0;
+
+    long double cir_center[2] = {0.0, 0.0};
+    long double cir_radius = 1200.0;
+
+    long double tri_center[2];
+    long double tri_len;
+
+    long double eight_center[2] = {0.0};
+    long double eight_len = 1400.0;
+    long double eight_radius = 1400.0;
+
+    long double vel_angle[OBJ_NUM]; // TO BE DONE: Return the angles of velocity vetors at a given point and a given direction
+
+    int direction[OBJ_NUM] = {CLOCKWISE};
+
+    enum traj {rect, cir, tri, playground, eight};
+    traj obj_traj[OBJ_NUM];
+    int obj_traj_seg[OBJ_NUM];
+
+    int corner_case[OBJ_NUM][OBJ_NUM] = {0};
+    int slow_car[OBJ_NUM][OBJ_NUM] = {0};
+
+    long double return_velocity_angle(int id long double x, long double y, long double vx, long double vy);
+    int unsafe_state_detector(int j, int k, long double jx, long double jy, long double jvx, long double jvy, long double kx, long double ky, long double kvx, long double kvy, long double coe);
+    }
